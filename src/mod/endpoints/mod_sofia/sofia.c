01343b2808f76f2210391298e762d0f7dbc4fce9
Ryan Colobong <ryanc@telnyx.com>
2020-04-01 13:37:13 +0800

    ENGDESK-4596: Unique-ID is included when adding channel vars

--- src/mod/endpoints/mod_sofia/sofia.c
+++ src/mod/endpoints/mod_sofia/sofia.c
@@ -709,7 +709,7 @@ void sofia_handle_sip_i_notify(switch_core_session_t *session, int status,
 		}
 		if (session) {
 			switch_channel_event_set_data(channel, s_event);
-			switch_event_add_header_string(s_event, SWITCH_STACK_BOTTOM, "Unique-ID", switch_core_session_get_uuid(session));
+			switch_event_add_header_string(s_event, SWITCH_STACK_BOTTOM, "UniqueID", switch_core_session_get_uuid(session));
 		}
 		switch_event_fire(&s_event);
 	}
