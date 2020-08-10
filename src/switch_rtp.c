a159c032b63ca776920d0745a0e109dc1e22f9c9
Ryan Colobong <ryanc@telnyx.com>
2020-08-11 01:12:58 +0800

    TEL-4547: Lock session during zrtp_event_callback

--- src/switch_rtp.c
+++ src/switch_rtp.c
@@ -1368,15 +1368,21 @@ static void zrtp_event_callback(zrtp_stream_t *stream, unsigned event)
 	switch_rtp_t *rtp_session = zrtp_stream_get_userdata(stream);
 	zrtp_session_info_t zrtp_session_info;
 
-	switch_channel_t *channel = switch_core_session_get_channel(rtp_session->session);
+	switch_channel_t *channel = NULL;
 	switch_event_t *fsevent = NULL;
 	const char *type;
 
+	if (switch_core_session_read_lock(rtp_session->session) != SWITCH_STATUS_SUCCESS) {
+		return;
+	}
+
+	channel = switch_core_session_get_channel(rtp_session->session);
 	type = rtp_type(rtp_session);
 
 	switch (event) {
 	case ZRTP_EVENT_IS_SECURE:
 		{
+			switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(rtp_session->session), SWITCH_LOG_DEBUG, "ZRTP is secured!\n");
 			rtp_session->flags[SWITCH_ZRTP_FLAG_SECURE_SEND] = 1;
 			rtp_session->flags[SWITCH_ZRTP_FLAG_SECURE_RECV] = 1;
 			if (!rtp_session->flags[SWITCH_RTP_FLAG_VIDEO]) {
@@ -1484,6 +1490,8 @@ static void zrtp_event_callback(zrtp_stream_t *stream, unsigned event)
 	default:
 		break;
 	}
+
+	switch_core_session_rwunlock(rtp_session->session);
 }
 
 static void zrtp_logger(int level, const char *data, int len, int offset)
