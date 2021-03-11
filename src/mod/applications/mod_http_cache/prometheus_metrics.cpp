2ea7795e6f06df02101300dcaf2b2f9d60798ecb
Sungtae Kim <sungtae@telnyx.com>
2021-03-11 23:30:50 +0900

    Fixed wrong name

--- src/mod/applications/mod_http_cache/prometheus_metrics.cpp
+++ src/mod/applications/mod_http_cache/prometheus_metrics.cpp
@@ -24,16 +24,16 @@ public:
 		_pool(pool),
 		_module_interface(module_interface),
 		_download_fail_count(0),
-		_download_bucker_0_299(0),
-		_download_bucker_300_499(0),
-		_download_bucker_500_799(0),
-		_download_bucker_800_1299(0),
-		_download_bucker_1300_2099(0),
-		_download_bucker_2100_3399(0),
-		_download_bucker_3400_5499(0),
-		_download_bucker_5500_inf(0),
-		_download_bucker_sum(0),
-		_download_bucker_count(0)
+		_download_bucket_0_299(0),
+		_download_bucket_300_499(0),
+		_download_bucket_500_799(0),
+		_download_bucket_800_1299(0),
+		_download_bucket_1300_2099(0),
+		_download_bucket_2100_3399(0),
+		_download_bucket_3400_5499(0),
+		_download_bucket_5500_inf(0),
+		_download_bucket_sum(0),
+		_download_bucket_count(0)
 	{
 		switch_mutex_init(&_mutex, SWITCH_MUTEX_NESTED, _pool);
 	}
@@ -47,25 +47,25 @@ public:
 	{
 		auto_lock lock(_mutex);
 
-		_download_bucker_sum += duration;
-		_download_bucker_count++;
+		_download_bucket_sum += duration;
+		_download_bucket_count++;
 
 		if (duration < 300) {
-			_download_bucker_0_299++;
+			_download_bucket_0_299++;
 		} else if (300 <= duration && duration < 500) {
-			_download_bucker_300_499++;
+			_download_bucket_300_499++;
 		} else if (500 <= duration && duration < 799) {
-			_download_bucker_500_799++;
+			_download_bucket_500_799++;
 		} else if (800 <= duration && duration < 1299) {
-			_download_bucker_800_1299++;
+			_download_bucket_800_1299++;
 		} else if (1300 <= duration && duration < 2099) {
-			_download_bucker_1300_2099++;
+			_download_bucket_1300_2099++;
 		} else if (2100 <= duration && duration < 3399) {
-			_download_bucker_2100_3399++;
+			_download_bucket_2100_3399++;
 		} else if (3400 <= duration && duration < 5499) {
-			_download_bucker_3400_5499++;
+			_download_bucket_3400_5499++;
 		} else {
-			_download_bucker_5500_inf++;
+			_download_bucket_5500_inf++;
 		} 
 	}
 
@@ -82,18 +82,18 @@ public:
 		stream->write_function(stream, "# TYPE mod_http_cache_download_fail_count counter\n");
 		stream->write_function(stream, "mod_http_cache_download_fail_count %u\n", _download_fail_count);
 
-		stream->write_function(stream, "# HELP mod_http_cache_download\n");
-		stream->write_function(stream, "# TYPE mod_http_cache_download histogram\n");
-		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"300\"} %u\n", _download_bucker_0_299);
-		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"500\"} %u\n", _download_bucker_300_499);
-		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"800\"} %u\n", _download_bucker_500_799);
-		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"1300\"} %u\n", _download_bucker_800_1299);
-		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"2100\"} %u\n", _download_bucker_1300_2099);
-		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"3400\"} %u\n", _download_bucker_2100_3399);
-		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"5500\"} %u\n", _download_bucker_3400_5499);
-		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"+Inf\"} %u\n", _download_bucker_5500_inf);
-		stream->write_function(stream, "mod_http_cache_download_duration_sum %llu\n", _download_bucker_sum);
-		stream->write_function(stream, "mod_http_cache_download_duration_count %u\n", _download_bucker_count);
+		stream->write_function(stream, "# HELP mod_http_cache_download_duration\n");
+		stream->write_function(stream, "# TYPE mod_http_cache_download_duration histogram\n");
+		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"300\"} %u\n", _download_bucket_0_299);
+		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"500\"} %u\n", _download_bucket_300_499);
+		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"800\"} %u\n", _download_bucket_500_799);
+		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"1300\"} %u\n", _download_bucket_800_1299);
+		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"2100\"} %u\n", _download_bucket_1300_2099);
+		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"3400\"} %u\n", _download_bucket_2100_3399);
+		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"5500\"} %u\n", _download_bucket_3400_5499);
+		stream->write_function(stream, "mod_http_cache_download_duration_bucket{le=\"+Inf\"} %u\n", _download_bucket_5500_inf);
+		stream->write_function(stream, "mod_http_cache_download_duration_sum %llu\n", _download_bucket_sum);
+		stream->write_function(stream, "mod_http_cache_download_duration_count %u\n", _download_bucket_count);
 	}
 	
 
@@ -105,16 +105,16 @@ private:
 	unsigned int _download_fail_count;
 
 	// counters for download duration
-	unsigned int _download_bucker_0_299;		// counter for 0 ~ 299 ms
-	unsigned int _download_bucker_300_499;		// counter for 300 ~ 499 ms
-	unsigned int _download_bucker_500_799;		// counter for 500 ~ 799 ms
-	unsigned int _download_bucker_800_1299;		// counter for 800 ~ 1299 ms
-	unsigned int _download_bucker_1300_2099;	// counter for 1300 ~ 2099 ms
-	unsigned int _download_bucker_2100_3399;	// counter for 2100 ~ 3399 ms
-	unsigned int _download_bucker_3400_5499;	// counter for 3400 ~ 5499 ms
-	unsigned int _download_bucker_5500_inf;		// counter for 5500 ~ inf
-	unsigned long long int _download_bucker_sum;
-	unsigned long int _download_bucker_count;
+	unsigned int _download_bucket_0_299;		// counter for 0 ~ 299 ms
+	unsigned int _download_bucket_300_499;		// counter for 300 ~ 499 ms
+	unsigned int _download_bucket_500_799;		// counter for 500 ~ 799 ms
+	unsigned int _download_bucket_800_1299;		// counter for 800 ~ 1299 ms
+	unsigned int _download_bucket_1300_2099;	// counter for 1300 ~ 2099 ms
+	unsigned int _download_bucket_2100_3399;	// counter for 2100 ~ 3399 ms
+	unsigned int _download_bucket_3400_5499;	// counter for 3400 ~ 5499 ms
+	unsigned int _download_bucket_5500_inf;		// counter for 5500 ~ inf
+	unsigned long long int _download_bucket_sum;
+	unsigned long int _download_bucket_count;
 };
 
 static prometheus_metrics* instance = 0;
