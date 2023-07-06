c02d166df21737c7d36bfe2db5e1cd5b1f5ab4ab
Emmanuel Balogun <engodunayo@gmail.com>
2023-07-06 19:01:04 +0100

    Minor changes

--- src/mod/asr_tts/mod_unimrcp/mod_unimrcp.c
+++ src/mod/asr_tts/mod_unimrcp/mod_unimrcp.c
@@ -1961,7 +1961,7 @@ static apt_bool_t speech_on_channel_add(mrcp_application_t *application, mrcp_se
 	return TRUE;
 
 error:
-	if (schannel->channel_destroyed) {
+	if (schannel && schannel->channel_destroyed) {
 		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Session already destroyed!\n");
 	} else if (schannel) {
 		switch_log_printf(SWITCH_CHANNEL_UUID_LOG(schannel->session_uuid), SWITCH_LOG_ERROR, "(%s) %s channel error!\n", schannel->name,
@@ -3789,15 +3789,16 @@ static apt_bool_t recog_on_message_receive(mrcp_application_t *application, mrcp
 static apt_bool_t recog_stream_open(mpf_audio_stream_t *stream, mpf_codec_t *codec)
 {
 	speech_channel_t *schannel = (speech_channel_t *) stream->obj;
+	recognizer_data_t *r;
 
-	if (!schannel){
-		return FALSE;
+	if (schannel){
+		r = (recognizer_data_t *) schannel->data;
+		r->unimrcp_stream = stream;
+
+		return TRUE;
 	}
 
-	recognizer_data_t *r = (recognizer_data_t *) schannel->data;
-	r->unimrcp_stream = stream;
-
-	return TRUE;
+	return FALSE;
 }
 
 /**
