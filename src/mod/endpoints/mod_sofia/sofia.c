9ec49b223d42fdcaa1affb1c9ce60564e1f291cb
Alfonso Pinto <alfonso@telnyx.com>
2024-01-17 20:27:12 +0100

    TEL-5885: fix DE-REGISTER

--- src/mod/endpoints/mod_sofia/sofia.c
+++ src/mod/endpoints/mod_sofia/sofia.c
@@ -1516,6 +1516,14 @@ static void our_sofia_event_callback(nua_event_t event,
 
 	if (sofia_private && sofia_private != &mod_sofia_globals.destroy_private && sofia_private != &mod_sofia_globals.keep_private) {
 		if (!zstr(sofia_private->gateway_name)) {
+			if (event == nua_r_unregister && status == 200) {
+				switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_DEBUG, "Destroy handle after unregister.\n");
+				sofia_private_free(sofia_private);
+				nua_handle_bind(nh, NULL);
+				nua_handle_destroy(nh);
+				nh = NULL;
+				return;
+			}
 			if (!(gateway = sofia_reg_find_gateway(sofia_private->gateway_name))) {
 				return;
 			}
