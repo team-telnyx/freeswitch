a2c7bb158e3ce4f082edf7e7f1102a8b957a720b
damirn <damir@telnyx.com>
2024-12-03 18:09:48 +0100

    TEL-5633 Implement new media bug recording flag RECORD_STEREO_NO_DOWN_MIX

--- src/switch_ivr_async.c
+++ src/switch_ivr_async.c
@@ -3127,6 +3127,20 @@ SWITCH_DECLARE(switch_status_t) switch_ivr_record_session_event(switch_core_sess
 			flags |= SMBF_STEREO_SWAP;
 			channels = 2;
 		}
+	} else if (channels == 2) {
+		flags |= SMBF_REAL_STEREO;
+		if (recording_var_true(channel, vars, "RECORD_STEREO")) {
+			flags |= SMBF_STEREO;
+			flags &= ~SMBF_STEREO_SWAP;
+		}
+
+		if (recording_var_true(channel, vars, "RECORD_STEREO_SWAP")) {
+			flags |= SMBF_STEREO;
+			flags |= SMBF_STEREO_SWAP;
+		}
+		if (!(flags & SMBF_STEREO)) {
+			channels = 1;
+		}
 	}
 
 	if (recording_var_true(channel, vars, "RECORD_ANSWER_REQ")) {
@@ -3135,6 +3149,10 @@ SWITCH_DECLARE(switch_status_t) switch_ivr_record_session_event(switch_core_sess
 
 	if (recording_var_true(channel, vars, "RECORD_BRIDGE_REQ")) {
 		flags |= SMBF_BRIDGE_REQ;
+	}
+
+	if (recording_var_true(channel, vars, "RECORD_STEREO_NO_DOWN_MIX")) {
+		flags |= SMBF_STEREO_NO_DOWN_MIX;
 	}
 
 	if (recording_var_true(channel, vars, "RECORD_APPEND")) {
