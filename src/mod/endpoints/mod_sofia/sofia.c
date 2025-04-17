d75d85a3658332072851a9d1208b52026938cdf2
Damir Nedzibovic <damir@telnyx.com>
2025-04-17 08:43:05 +0200

    TEL-6385: Allow SIP challenge if channel variables sip_auth_username and sip_auth_password are present.

--- src/mod/endpoints/mod_sofia/sofia.c
+++ src/mod/endpoints/mod_sofia/sofia.c
@@ -1655,6 +1655,14 @@ static void our_sofia_event_callback(nua_event_t event,
 	}
 
 	if (sip && (status == 401 || status == 407)) {
+		if (channel) {
+			const char *sip_auth_username = switch_channel_get_variable(channel, "sip_auth_username");
+			const char *sip_auth_password = switch_channel_get_variable(channel, "sip_auth_password");
+			if (!zstr(sip_auth_username) && !zstr(sip_auth_password)) {
+				sofia_reg_handle_sip_r_challenge(status, phrase, nua, profile, nh, sofia_private, session, gateway, sip, de, tags);
+				goto done;
+			}
+		}
 		if (!sofia_test_pflag(profile, PFLAG_DISABLE_AUTH_CHALLENGE_RESPONSE)) {
 			sofia_reg_handle_sip_r_challenge(status, phrase, nua, profile, nh, sofia_private, session, gateway, sip, de, tags);
 		} else {
