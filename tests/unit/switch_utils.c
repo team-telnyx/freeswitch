52fcb4fe2b6e5eca53c08f53aa84093af4f616d2
Damir Nedzibovic <damir@telnyx.com>
2025-04-21 12:35:50 +0200

    TEL-6387: Add more tests.

--- tests/unit/switch_utils.c
+++ tests/unit/switch_utils.c
@@ -84,6 +84,10 @@ FST_TEST_BEGIN(is_file_path)
 {
 	switch_bool_t b = switch_is_file_path("{av_record_audio_only=true");
 	fst_requires(b == SWITCH_FALSE);
+	b = switch_is_file_path("{av_record_audio_only=true,future_json_var='{key=value}'");
+	fst_requires(b == SWITCH_FALSE);
+	b = switch_is_file_path("{av_record_audio_only=true,future_json_var='{key=value}'}");
+	fst_requires(b == SWITCH_FALSE);
 }
 FST_TEST_END()
 
