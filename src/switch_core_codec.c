07601d456b05bb7abc907e229842205376b8a1f4
Damir Nedzibovic <damir@telnyx.com>
2025-06-04 16:51:58 +0200

    TEL-6349: Turn this into a INFO log and add mismatched fmtp's to it.

--- src/switch_core_codec.c
+++ src/switch_core_codec.c
@@ -708,7 +708,7 @@ SWITCH_DECLARE(switch_status_t) switch_core_codec_init_with_bitrate(switch_codec
 					implementation = iptr;
 					break;
 				}
-				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "Fmtp match failed.\n");
+				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "Fmtp match failed (%s - %s).\n", iptr->fmtp, fmtp);
 			} else {
 				implementation = iptr;
 				break;
