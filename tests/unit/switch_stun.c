d99c37f2a0090d555ebe192837d98a1e1002e744
Alfonso Pinto <alfonso@telnyx.com>
2025-06-19 15:33:11 +0200

    TEL-6410: stun introduce unit-test file

---
+++ tests/unit/switch_stun.c
@@ -0,0 +1,34 @@
+#include <switch.h>
+#include <switch_stun.h>
+#include <test/switch_test.h>
+
+
+FST_CORE_BEGIN("./conf_stun")
+{
+FST_SUITE_BEGIN(switch_stun)
+{
+FST_SETUP_BEGIN()
+{
+}
+FST_SETUP_END()
+
+FST_TEARDOWN_BEGIN()
+{
+}
+FST_TEARDOWN_END()
+	FST_TEST_BEGIN(test_binding_indication)
+	{
+		switch_stun_packet_t *packet;
+		/*   STUN Indication:  A STUN message that does not receive a response. */
+		static uint8_t incoming[] =
+			"\x00\x11\x00\x08\x21\x12\xa4\x42\x36\xde\x03\x48\x25\xa7\x69\x43\x31\x18\x96\x90\x80\x28\x00\x04\x07\x2c\xa6\xb4";
+
+		packet = switch_stun_packet_parse(incoming, sizeof(incoming) - 1);
+
+		switch_assert(packet);
+	}
+	FST_TEST_END()
+}
+FST_SUITE_END()
+}
+FST_CORE_END()
