6ad1aa53be18664f8ad8d9c536c09b2fe6f4ba99
damirn <damir@telnyx.com>
2026-04-15 14:26:33 +0200

    ENGDESK-48201: Pace outbound RFC 2833 against wall clock in do_2833. (#556)

--- src/switch_rtp.c
+++ src/switch_rtp.c
@@ -261,6 +261,7 @@ struct switch_rtp_rfc2833_data {
 	unsigned int out_digit_sofar;
 	unsigned int out_digit_sub_sofar;
 	unsigned int out_digit_dur;
+	switch_time_t out_digit_last_progress_us; /* gate against sub-ptime do_2833() wakeups; see ENGDESK-48201 */
 	uint16_t in_digit_seq;
 	uint32_t in_digit_ts;
 	uint32_t last_in_digit_ts;
@@ -6470,6 +6471,25 @@ SWITCH_DECLARE(void) do_2833(switch_rtp_t *rtp_session)
 
 	if (rtp_session->dtmf_data.out_digit_dur > 0) {
 		int x, loops = 1;
+		switch_time_t now_us = switch_micro_time_now();
+
+		/* ENGDESK-48201: drop calls less than half a ptime since the last in-flight progress
+		   so a tight-looping read path (e.g. silent peer + SIG_BREAK) can't collapse the digit. */
+		if (rtp_session->dtmf_data.out_digit_last_progress_us &&
+			rtp_session->samples_per_second &&
+			rtp_session->samples_per_interval) {
+			switch_time_t min_us =
+				((switch_time_t) rtp_session->samples_per_interval * 1000000)
+				/ rtp_session->samples_per_second / 2;
+
+			if ((now_us - rtp_session->dtmf_data.out_digit_last_progress_us) < min_us) {
+				switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(rtp_session->session), SWITCH_LOG_DEBUG,
+								  "Skipping do_2833 tick: %" SWITCH_TIME_T_FMT "us since last progress < %" SWITCH_TIME_T_FMT "us min\n",
+								  now_us - rtp_session->dtmf_data.out_digit_last_progress_us, min_us);
+				return;
+			}
+		}
+		rtp_session->dtmf_data.out_digit_last_progress_us = now_us;
 
 		if (!rtp_session->last_write_ts) {
 			if (rtp_session->timer.timer_interface) {
@@ -6616,6 +6636,7 @@ SWITCH_DECLARE(void) do_2833(switch_rtp_t *rtp_session)
 			rtp_session->dtmf_data.out_digit_sub_sofar = samples;
 			rtp_session->dtmf_data.out_digit_dur = rdigit->duration;
 			rtp_session->dtmf_data.out_digit = rdigit->digit;
+			rtp_session->dtmf_data.out_digit_last_progress_us = switch_micro_time_now(); /* seed gate */
 			rtp_session->dtmf_data.out_digit_packet[0] = (unsigned char) switch_char_to_rfc2833(rdigit->digit);
 			rtp_session->dtmf_data.out_digit_packet[1] = 13;
 			rtp_session->dtmf_data.out_digit_packet[2] = (unsigned char) (rtp_session->dtmf_data.out_digit_sub_sofar >> 8);
